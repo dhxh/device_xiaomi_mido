b09a91ce642b4d1105b35d8247193459d8d0b6cd
Rafiester <52811828+Damar131@users.noreply.github.com>
2019-10-25 22:34:36 +0700

    mido: Enable USB Debugging on boot
    easily take log from adb :p
    
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- device.mk
+++ device.mk
@@ -406,3 +406,10 @@ PRODUCT_PACKAGES += \
 # GCam Go
 PRODUCT_PACKAGES += \
     GCam
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
